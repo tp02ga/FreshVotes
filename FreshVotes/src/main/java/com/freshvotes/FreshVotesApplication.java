d826bdf79d0259857ed839310acacbee6fc6e300
Trevor Page <trevor@craftycodr.com>
2018-07-21 11:29:05 -0400

    initial check-in of the FreshVotes code

---
+++ FreshVotes/src/main/java/com/freshvotes/FreshVotesApplication.java
@@ -0,0 +1,12 @@
+package com.freshvotes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreshVotesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FreshVotesApplication.class, args);
+	}
+}
