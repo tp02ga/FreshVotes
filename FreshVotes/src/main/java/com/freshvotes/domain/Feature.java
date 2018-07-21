9b5a2282448731d75108db1ae4c7895245d7d4d6
Trevor Page <trevor@craftycodr.com>
2018-07-21 12:55:51 -0400

    adding the first of the domain objects

---
+++ FreshVotes/src/main/java/com/freshvotes/domain/Feature.java
@@ -0,0 +1,49 @@
+package com.freshvotes.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Feature
+{
+  private Long id;
+  private String title;
+  private String description;
+  private String status;
+  
+  @Id @GeneratedValue(strategy=GenerationType.IDENTITY)
+  public Long getId()
+  {
+    return id;
+  }
+  public void setId(Long id)
+  {
+    this.id = id;
+  }
+  public String getTitle()
+  {
+    return title;
+  }
+  public void setTitle(String title)
+  {
+    this.title = title;
+  }
+  public String getDescription()
+  {
+    return description;
+  }
+  public void setDescription(String description)
+  {
+    this.description = description;
+  }
+  public String getStatus()
+  {
+    return status;
+  }
+  public void setStatus(String status)
+  {
+    this.status = status;
+  }
+}
---
+++ FreshVotes/src/main/java/com/freshvotes/domain/User.java
@@ -0,0 +1,51 @@
+package com.freshvotes.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="users")
+public class User
+{
+  private Long id;
+  private String username;
+  private String password;
+  private String name;
+  
+  @Id @GeneratedValue(strategy=GenerationType.IDENTITY)
+  public Long getId()
+  {
+    return id;
+  }
+  public void setId(Long id)
+  {
+    this.id = id;
+  }
+  public String getUsername()
+  {
+    return username;
+  }
+  public void setUsername(String username)
+  {
+    this.username = username;
+  }
+  public String getPassword()
+  {
+    return password;
+  }
+  public void setPassword(String password)
+  {
+    this.password = password;
+  }
+  public String getName()
+  {
+    return name;
+  }
+  public void setName(String name)
+  {
+    this.name = name;
+  }
+}
