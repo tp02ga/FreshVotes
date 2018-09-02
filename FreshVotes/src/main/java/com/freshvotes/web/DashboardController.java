30d079b94a1d0b2e0d49e33073fd2974c0d84db5
Trevor Page <trevor@craftycodr.com>
2018-09-02 10:44:22 -0400

    Creating custom login page

---
+++ FreshVotes/src/main/java/com/freshvotes/web/DashboardController.java
@@ -0,0 +1,13 @@
+package com.freshvotes.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class DashboardController {
+  
+  @GetMapping("/")
+  public String rootView () {
+    return "index";
+  }
+}
