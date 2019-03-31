06f8e81f51065f017cef63fb7a70e947ee7fde62
Trevor Page <trevor@craftycodr.com>
2019-03-31 10:27:23 -0400

    Creating a header navbar fragment

--- FreshVotes/src/main/java/com/freshvotes/security/WebSecurityConfiguration.java
+++ FreshVotes/src/main/java/com/freshvotes/security/WebSecurityConfiguration.java
@@ -35,6 +35,7 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
         .authorizeRequests()
           .antMatchers("/").permitAll()
           .antMatchers("/register").permitAll()
+          .antMatchers("/images/**").permitAll()
           .antMatchers("/admin/**").hasRole("ADMIN")
           .anyRequest().hasRole("USER").and()
         .formLogin()
