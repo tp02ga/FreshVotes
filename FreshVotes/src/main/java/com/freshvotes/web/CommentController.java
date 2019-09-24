d1b947733ecc271f1d1918b190666d39b8b7ec36
Trevor Page <trevor@craftycodr.com>
2019-09-24 14:46:40 -0400

    Finishing Comments Functionality

--- FreshVotes/src/main/java/com/freshvotes/web/CommentController.java
+++ FreshVotes/src/main/java/com/freshvotes/web/CommentController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.freshvotes.domain.Comment;
@@ -38,8 +39,17 @@ public class CommentController {
   
   @PostMapping("")
   public String postComment(@AuthenticationPrincipal User user, @PathVariable Long productId,
-      @PathVariable Long featureId, Comment comment) {
+      @PathVariable Long featureId, Comment comment, @RequestParam(required=false) Long parentId,
+      @RequestParam(required=false) String childCommentText) {
     Optional<Feature> featureOpt = featureRepo.findById(featureId);
+    
+    if (parentId != null) {
+      comment = new Comment();
+      Optional<Comment> parentCommentOpt = commentRepo.findById(parentId);
+      if (parentCommentOpt.isPresent())
+        comment.setComment(parentCommentOpt.get());
+      comment.setText(childCommentText);
+    } 
     
     if (featureOpt.isPresent())
       comment.setFeature(featureOpt.get());
